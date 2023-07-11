686e65de7b319ae6871828aea1e311dd84a24164
Alexander <Alexander29_90@icloud.com>
2023-07-11 21:29:56 +0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Александр on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
