5a44efaa1d705b2f6a6b0ecca292e95536a6d481
Alexander <Alexander29_90@icloud.com>
2023-07-13 18:09:49 +0500

    Added struct Game

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Александр on 13.07.2023.
+//
+
+import Foundation
+
+struct Game {
+  var target = 37
+  var score = 0
+  var round = 1
+  
+  func points(sliderValue: Int) -> Int {
+    return 999
+  }
+}
